a52537d129e21e0c1a5e53f367572107621e576b
shindekalpesharun <shindekalpesharun@gmail.com>
2021-05-26 12:30:07 +0530

    added to host-web

--- lib/cubits/app_bar/app_bar_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-class AppBarCubit extends Cubit<double> {
-  AppBarCubit() : super(0);
-
-  void setoffSet(double offset) => emit(offset);
-}
-
-// event->bloc->state
